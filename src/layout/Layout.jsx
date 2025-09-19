b51deac959fdd37066d9ee5a9cd388d04822fee0
guiss26 <guissella584@gmail.com>
2025-09-18 23:24:16 -0700

    feat(layout): hide navbar on the home page (#5)

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import "./Layout.css"
 const Layout = () => {
+    const location = useLocation()
+
     return (
         <>
-            <Navbar></Navbar>
+            {/* <Navbar></Navbar> */}
+            {/*Mostrar Navbar en todas menos en "/*/}
+            {location.pathname !== "/" && <Navbar></Navbar>}
             <div className="container-section">
                 <Outlet></Outlet>
             </div>
